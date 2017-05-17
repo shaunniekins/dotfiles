870b72a733b2a6d9b2e7239989f6ca5fd5fd1e47
mou <mehdi@mehd.io>
2017-05-17 15:59:44 +0200

    change default TERM

--- full_install.sh
+++ full_install.sh
@@ -118,6 +118,5 @@ fc-cache -vf ~/.fonts/
 # Colors
 wget https://raw.githubusercontent.com/vim-scripts/wombat256.vim/master/colors/wombat256mod.vim
 mkdir -p $HOME/.vim/colors && mv wombat256mod.vim $HOME/.vim/colors/
-export TERM=xterm-256color
 ln -s $DOTFILES/tmux/tmux.conf.symlink $HOME/.tmux.conf
 
